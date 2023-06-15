09e9b1eef3aa3264d4d2989c302b9c176d70c7ea
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-15 16:14:08 -0300

    docs - // added eslint-disable-next-line to ignore a warning that says "const data set but not used".

--- src/utils/HourConverter.js
+++ src/utils/HourConverter.js
@@ -1,5 +1,6 @@
 const HourConverter = (fullDate) => {
     const dateTime = fullDate;
+    // eslint-disable-next-line
     const [date, time] = dateTime.split(" ");
     const hours = time.split(":").slice(0, 2).join(":");
     return hours;
