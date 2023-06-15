2e02a4d46c3a6195301e160f536b964fc7128bfb
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-15 16:21:25 -0300

    feat - new "Loading" component, made to appear on the screen while the app handles the API call.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  animation: spin 2s linear infinite;
+  margin: 50px;
+
+  transition: all 0.2s ease-in-out;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Loading = () => {
+  return <StyledDiv />;
+};
+
+export default Loading;
