e7420477399813f32a0e16f8f4f4666833f876d8
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-15 16:19:52 -0300

    style - Widget background color changed to a better color, horizontal line changed to white.

--- src/components/Div.jsx
+++ src/components/Div.jsx
@@ -6,17 +6,20 @@ export const FlexDiv = styled.div`
   align-items: center;
   margin-top: 30px;
   gap: 20px;
+  transition: all 0.2s ease-in-out;
 `;
 
 export const Widget = styled.div`
   max-width: 648px;
   max-height: 760px;
   padding: 20px;
-  background: rgba(42, 6, 112, 0.613) 0%;
+  background: rgb(31, 110, 140) 0%;
 
   backdrop-filter: blur(25px);
   border-radius: 20px;
   box-shadow: inset 0px 0px 30px 10px rgb(2, 240, 255, 0.1);
+
+  transition: all 0.2s ease-in-out;
 `;
 
 export const WidgetContent = styled.div`
@@ -28,6 +31,6 @@ export const WidgetContent = styled.div`
 export const Divider = styled.div`
   width: 100%;
   height: 1px;
-  background-color: black;
+  background-color: #fff;
   margin: 30px 0;
 `;
