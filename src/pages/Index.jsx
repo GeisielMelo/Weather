3f7c608a9de39ee3ea9abdc2ffa6f806d8f024db
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-15 16:30:13 -0300

    feat - some changes here. Removed the app's static data and replaced it with a dynamic one using geolocation.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Widget, WidgetContent, FlexDiv, Divider } from "../components/Div";
 import Title from "../components/Title";
 import Temperature from "../components/Temperature";
 import Weather from "../components/Weather";
@@ -8,39 +9,94 @@ import DayCycle from "../components/DayCycle";
 import HourConverter from "../utils/HourConverter";
 import KelvinToCelsius from "../utils/TemperatureConverter";
 import TimestampToHour from "../utils/TimestampConverter";
-import { Widget, WidgetContent, FlexDiv, Divider } from "../components/Div";
-import { todayData, nextHoursData, sunData, localData } from "../utils/Data";
+import Loading from "../components/Loading";
+import getUserLocation from "../services/localDeviceService";
+import getWeatherData from "../services/openWeatherService";
 
 const Home = () => {
+  const [location, setLocation] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserLocation = async () => {
+      try {
+        const location = await getUserLocation();
+        setLocation(location);
+      } catch (error) {
+        console.error("Geolocation request rejected.");
+        setLocation({ lat: -15.7801, lon: -47.9292 });
+      }
+    };
+
+    fetchUserLocation();
+  }, []);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        if (location) {
+          const weatherData = await getWeatherData(location.lat, location.lon);
+          setData(weatherData);
+        }
+      } catch (error) {
+        console.error("Error on fetch weather data.");
+        setData(null);
+      }
+    };
+
+    fetchWeatherData();
+  }, [location]);
+
+  const cityName = data?.city?.name;
+  const temperature = data ? data?.list[0].main.temp : null;
+  const weather = data ? data?.list[0].weather[0].main : null;
+  const sunrise = data ? data?.city?.sunrise : null;
+  const sunset = data ? data?.city?.sunset : null;
+  const feelsLike = data ? data?.list[0].main.feels_like : null;
+  const humidity = data ? data?.list[0].main.humidity : null;
+  const wind = data ? data?.list[0].wind.speed : null;
+  const pressure = data ? data?.list[0].main.pressure : null;
+
   return (
     <Widget>
       <WidgetContent>
-        <Title title={todayData.city} />
-        <Temperature temperature={KelvinToCelsius(todayData.temperature)} />
-        <Weather weather={todayData.weather} />
-        <FlexDiv>
-          {nextHoursData.map((item) => (
-            <NextHours
-              maxTemp={KelvinToCelsius(item.max)}
-              hour={HourConverter(item.time)}
-              icon={item.icon}
-            />
-          ))}
-        </FlexDiv>
-        <Divider />
-        <DayCycle
-          sunriseTime={TimestampToHour(sunData.sunrise)}
-          sunsetTime={TimestampToHour(sunData.sunset)}
-        />
-        <FlexDiv>
-          <Info
-            info={KelvinToCelsius(localData.feelsLike) + "°"}
-            description="RealFeel"
-          />
-          <Info info={localData.humidity + "%"} description="Humidity" />
-          <Info info={localData.windSpeed} description="Wind" />
-          <Info info={localData.pressure} description="Pressure" />
-        </FlexDiv>
+        {data ? (
+          <>
+            <Widget>
+              <WidgetContent>
+                <Title title={cityName} />
+                <Temperature temperature={KelvinToCelsius(temperature)} />
+                <Weather weather={weather} />
+                <FlexDiv>
+                  {data?.list.slice(3, 9).map((item) => (
+                    <NextHours
+                      maxTemp={KelvinToCelsius(item.main.temp)}
+                      hour={HourConverter(item.dt_txt)}
+                      icon={item.weather[0].icon}
+                      key={item.dt_txt}
+                    />
+                  ))}
+                </FlexDiv>
+                <Divider />
+                <DayCycle
+                  sunriseTime={TimestampToHour(sunrise)}
+                  sunsetTime={TimestampToHour(sunset)}
+                />
+                <FlexDiv>
+                  <Info
+                    info={KelvinToCelsius(feelsLike) + "°"}
+                    description="RealFeel"
+                  />
+                  <Info info={humidity + "%"} description="Humidity" />
+                  <Info info={wind} description="Wind" />
+                  <Info info={pressure} description="Pressure" />
+                </FlexDiv>
+              </WidgetContent>
+            </Widget>
+          </>
+        ) : (
+          <Loading />
+        )}
       </WidgetContent>
     </Widget>
   );
