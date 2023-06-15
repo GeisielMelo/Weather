813c0a2169f66854388b46054a9c81fc35fc7015
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-14 21:25:49 -0300

    feat - New module that takes a timestamp value and converts it to a time.

---
+++ src/utils/TimestampConverter.js
@@ -0,0 +1,9 @@
+const TimestampToHour = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const time = `${hour}:${minutes}`;
+  return time;
+};
+
+export default TimestampToHour;
