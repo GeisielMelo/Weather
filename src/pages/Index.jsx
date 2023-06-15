8d63473a83d95bcf8a147b303beb9a4f3c49bb68
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-15 19:45:06 -0300

    chore - renamed const [data, setData] to [forecastData, setForecastData], a new useEffect to handle the new weather data.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,11 +11,12 @@ import KelvinToCelsius from "../utils/TemperatureConverter";
 import TimestampToHour from "../utils/TimestampConverter";
 import Loading from "../components/Loading";
 import getUserLocation from "../services/localDeviceService";
-import getWeatherData from "../services/openWeatherService";
+import { getForecast, getWeather } from "../services/openWeatherService";
 
 const Home = () => {
   const [location, setLocation] = useState(null);
-  const [data, setData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
     const fetchUserLocation = async () => {
@@ -32,35 +33,51 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
+    const fetchForecastData = async () => {
+      try {
+        if (location) {
+          const forecastData = await getForecast(location.lat, location.lon);
+          setForecastData(forecastData);
+        }
+      } catch (error) {
+        console.error("Error on fetch forecast data.");
+        setForecastData(null);
+      }
+    };
+
+    fetchForecastData();
+  }, [location]);
+
+  useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         if (location) {
-          const weatherData = await getWeatherData(location.lat, location.lon);
-          setData(weatherData);
+          const weatherData = await getWeather(location.lat, location.lon);
+          setWeatherData(weatherData);
         }
       } catch (error) {
         console.error("Error on fetch weather data.");
-        setData(null);
+        setWeatherData(null);
       }
     };
 
     fetchWeatherData();
   }, [location]);
 
-  const cityName = data?.city?.name;
-  const temperature = data ? data?.list[0].main.temp : null;
-  const weather = data ? data?.list[0].weather[0].main : null;
-  const sunrise = data ? data?.city?.sunrise : null;
-  const sunset = data ? data?.city?.sunset : null;
-  const feelsLike = data ? data?.list[0].main.feels_like : null;
-  const humidity = data ? data?.list[0].main.humidity : null;
-  const wind = data ? data?.list[0].wind.speed : null;
-  const pressure = data ? data?.list[0].main.pressure : null;
+  const cityName = weatherData?.name;
+  const temperature = weatherData ? weatherData?.main.temp : null;
+  const weather = weatherData ? weatherData?.weather[0].main : null;
+  const sunrise = weatherData ? weatherData?.sys.sunrise : null;
+  const sunset = weatherData ? weatherData?.sys.sunset : null;
+  const feelsLike = weatherData ? weatherData?.main.feels_like : null;
+  const humidity = weatherData ? weatherData?.main.humidity : null;
+  const wind = weatherData ? weatherData?.wind.speed : null;
+  const pressure = weatherData ? weatherData?.main.pressure : null;
 
   return (
     <Widget>
       <WidgetContent>
-        {data ? (
+        {forecastData && weatherData ? (
           <>
             <Widget>
               <WidgetContent>
@@ -68,7 +85,7 @@ const Home = () => {
                 <Temperature temperature={KelvinToCelsius(temperature)} />
                 <Weather weather={weather} />
                 <FlexDiv>
-                  {data?.list.slice(3, 9).map((item) => (
+                  {forecastData?.list.slice(0, 6).map((item) => (
                     <NextHours
                       maxTemp={KelvinToCelsius(item.main.temp)}
                       hour={HourConverter(item.dt_txt)}
