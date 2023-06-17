ce4560003e1cdc0488eb05d8101f86fd4117c6c2
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-16 23:29:33 -0300

    feat - a new function to search data using city and country as parameter.

--- src/services/openWeatherService.js
+++ src/services/openWeatherService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+
 const apiKey = "";
+const apiForecast = "https://api.openweathermap.org/data/2.5/forecast?";
+const apiWeather = "https://api.openweathermap.org/data/2.5/weather?";
 
-const getWeatherData = (latitude, longitude, forecastType) => {
-  const apiForecast = "https://api.openweathermap.org/data/2.5/forecast?";
-  const apiWeather = "https://api.openweathermap.org/data/2.5/weather?";
-
+export const searchByCoordinates = (latitude, longitude, forecastType) => {
   const lat = `lat=${latitude}`;
   const lon = `lon=${longitude}`;
 
@@ -30,5 +30,24 @@ const getWeatherData = (latitude, longitude, forecastType) => {
   });
 };
 
-export default getWeatherData;
-
+export const searchByCity = (city, country, forecastType) => {
+  const cityName = `q=${city},${country}`;
+  let url = "";
+  if (forecastType === "weather") {
+    url = `${apiWeather}${cityName}&appid=${apiKey}`;
+  } else if (forecastType === "forecast") {
+    url = `${apiForecast}${cityName}&appid=${apiKey}`;
+  }
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((response) => {
+        const weatherData = response.data;
+        resolve(weatherData);
+      })
+      .catch((error) => {
+        console.error("Error in getting weather data.");
+        reject(error);
+      });
+  });
+};
